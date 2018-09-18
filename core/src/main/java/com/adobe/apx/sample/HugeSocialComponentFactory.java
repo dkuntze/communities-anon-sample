7d8a23177f9346d62fd3ba70f787d16bdb571e55
Darin Kuntze <dkuntze@adobe.com>
2018-09-18 13:42:45 -0500

    add the client util methods from a buried abstract class to stop the null pointers

--- core/src/main/java/com/adobe/apx/sample/HugeSocialComponentFactory.java
+++ core/src/main/java/com/adobe/apx/sample/HugeSocialComponentFactory.java
@@ -3,14 +3,17 @@ package com.adobe.apx.sample;
 import com.adobe.cq.social.commons.comments.api.Comment;
 import com.adobe.cq.social.commons.comments.api.CommentSystemSocialComponentFactory;
 import com.adobe.cq.social.commons.comments.listing.CommentSocialComponentListProviderManager;
-import com.adobe.cq.social.scf.ClientUtilities;
-import com.adobe.cq.social.scf.QueryRequestInfo;
-import com.adobe.cq.social.scf.SocialComponent;
-import com.adobe.cq.social.scf.SocialComponentFactory;
+import com.adobe.cq.social.scf.*;
+import com.adobe.cq.social.ugcbase.SocialUtils;
+import com.adobe.granite.xss.XSSAPI;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,13 +29,27 @@ public class HugeSocialComponentFactory extends CommentSystemSocialComponentFact
     @Reference
     private CommentSocialComponentListProviderManager commentSocialComponentListProviderManager;
 
+    @Reference
+    private ResourceResolverFactory resolverFactory;
+
+    @Reference
+    ClientUtilityFactory clientUtilFactory;
+
+    @Reference
+    private SocialUtils socialUtils;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
+    private XSSAPI xss;
+
 
     @Override
     public SocialComponent getSocialComponent(Resource resource) {
-        log.info("GSC1");
+        log.debug("GSC1");
         try {
-            log.info("isLive: " + resource.getResourceResolver().getUserID());
-            return new HugeComment(resource, getClientUtilities(resource.getResourceResolver()), commentSocialComponentListProviderManager);
+            log.debug("GSC1 isLive: " + resource.getResourceResolver().getUserID());
+            HugeComment returnComment = new HugeComment(resource, getClientUtilities(resource.getResourceResolver()), commentSocialComponentListProviderManager);
+            log.debug("GSC1 returning comment");
+            return returnComment;
         } catch (Exception e) {
             log.error(e.getMessage());
             return null;
@@ -41,10 +58,13 @@ public class HugeSocialComponentFactory extends CommentSystemSocialComponentFact
 
     @Override
     public SocialComponent getSocialComponent(Resource resource, SlingHttpServletRequest slingHttpServletRequest) {
-        log.info("GSC2");
+        log.debug("GSC2 getSocialComponent");
         try {
-            log.info(resource.getPath());
-            return new HugeComment(resource, getClientUtilities(slingHttpServletRequest), commentSocialComponentListProviderManager);
+            log.debug(getClientUtilities(slingHttpServletRequest).getAuthorizedUserId());
+            HugeComment hugeComment = new HugeComment(resource, getClientUtilities(slingHttpServletRequest), commentSocialComponentListProviderManager);
+
+            log.debug(hugeComment.getEmail());
+            return hugeComment;
         } catch (Exception e) {
             log.error(e.getMessage());
             return null;
@@ -67,7 +87,15 @@ public class HugeSocialComponentFactory extends CommentSystemSocialComponentFact
         return 10;
     }
 
-    public String getSupportedResourceType() {
+    protected ClientUtilities getClientUtilities(final SlingHttpServletRequest request) {
+        return clientUtilFactory.getClientUtilities(xss, request, socialUtils);
+    }
+
+    protected ClientUtilities getClientUtilities(final ResourceResolver resourceResolver) {
+        return clientUtilFactory.getClientUtilities(this.xss, resourceResolver, socialUtils);
+    }
+
+        public String getSupportedResourceType() {
         return Comment.COMMENT_RESOURCETYPE;
     }
 
