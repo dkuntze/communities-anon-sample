38b46c8a70c6ad09426ebff6be2863bea5a67ff8
Darin Kuntze <dkuntze@adobe.com>
2018-09-24 13:52:34 -0500

    add the create override

--- core/src/main/java/com/adobe/apx/sample/operation/impl/HugeCommentOperationService.java
+++ core/src/main/java/com/adobe/apx/sample/operation/impl/HugeCommentOperationService.java
@@ -1,17 +1,18 @@
 package com.adobe.apx.sample.operation.impl;
 
-import com.adobe.aemds.guide.utils.JcrResourceConstants;
+import com.adobe.cq.social.commons.CommentException;
 import com.adobe.cq.social.commons.CommentSystem;
+import com.adobe.cq.social.commons.bundleactivator.Activator;
 import com.adobe.cq.social.commons.comments.api.Comment;
 import com.adobe.cq.social.commons.comments.endpoints.AbstractCommentOperationService;
 import com.adobe.cq.social.commons.comments.endpoints.CommentOperationExtension;
 import com.adobe.cq.social.commons.comments.endpoints.CommentOperations;
 import com.adobe.cq.social.commons.events.CommentEvent;
+import com.adobe.cq.social.commons.ugclimiter.api.UGCLimiterService;
+import com.adobe.cq.social.scf.OperationException;
 import com.adobe.cq.social.scf.SocialComponent;
 import com.adobe.cq.social.scf.SocialComponentFactory;
 import com.adobe.cq.social.scf.SocialComponentFactoryManager;
-import com.adobe.cq.social.scf.core.SocialEvent;
-import com.adobe.cq.social.ugcbase.CollabUser;
 import com.adobe.cq.social.ugcbase.SocialUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.LoginException;
@@ -23,7 +24,13 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.activation.DataSource;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component(
         service = CommentOperations.class,
@@ -49,6 +56,8 @@ public class HugeCommentOperationService extends AbstractCommentOperationService
     @Reference
     private ResourceResolverFactory resolverFactory;
 
+    private static final String ANONYMOUS = "anonymous";
+
     protected Comment getSocialComponentForResource(final Resource resource) {
         if (resource == null) {
             return null;
@@ -64,7 +73,7 @@ public class HugeCommentOperationService extends AbstractCommentOperationService
 
     protected void postCreateEvent(final com.adobe.cq.social.commons.comments.api.Comment comment, final String userId) {
 
-        //LOG.info("something is null here: " + comment.getMessage());
+        LOG.info("something is null here: " + userId);
         try {
             postEvent(new CommentEvent(comment, userId, comment.isTopLevel() ? CommentEvent.CommentActions.CREATED
                     : CommentEvent.CommentActions.REPLIED));
@@ -129,6 +138,55 @@ public class HugeCommentOperationService extends AbstractCommentOperationService
 
     protected String getResourceType(Resource resource) {
         return com.adobe.cq.social.commons.comments.api.Comment.COMMENT_RESOURCETYPE;
+    }
+
+    public Resource create(final SlingHttpServletRequest request, final Session session) throws OperationException {
+        final Resource resource = request.getResource();
+
+        final CommentSystem cs = getCommentSystem(resource, session);
+
+        validateParameters(request);
+
+        final String name = "admin"; //set this to something other than anonymous to post
+        if (name.equals(ANONYMOUS)) {
+            throw new OperationException("Session is invalid",
+                    HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+        if (ugcLimiterService == null) {
+            // this throws IllegalStateException if reference can't be found for the service
+            ugcLimiterService = Activator.getService(UGCLimiterService.class);
+        }
+
+        if (!ugcLimiterService.mayCreateUGC(request.getResourceResolver(), name)) {
+            // Dont change the exception message. This exception is used in UI and it may stop working
+            // Refer to CQ-97142
+            throw new OperationException("Exceeded contribution limit of " + ugcLimiterService.getUgcLimit()
+                    + " pieces of content per 500. " + 500,
+                    HttpServletResponse.SC_FORBIDDEN);
+        }
+
+        if (!mayPost(request, cs, name)) {
+            throw new OperationException("User not allowed to post to forum at " + cs.getPath(),
+                    HttpServletResponse.SC_PRECONDITION_FAILED);
+        }
+
+        final Map<String, Object> props = new HashMap<>();
+        try {
+            getDefaultProperties(request, props, session);
+            getCustomProperties(request, props, session);
+        } catch (final CommentException e) {
+            throw new OperationException("Failed to create comment", e,
+                    HttpServletResponse.SC_NON_AUTHORITATIVE_INFORMATION);
+        } catch (final RepositoryException e) {
+            throw new OperationException("Failed to get comment properties", e,
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+        // create comment
+        final List<DataSource> attachments = getAttachmentsFromRequest(request, cs);
+        return create(resource, cs, name, props, attachments, session);
+
     }
 
 
